5baea98a833ae84f07140c84277e58cf688686cf
Sriramsai045 <ss3110@srmist.edu.in>
2026-02-12 16:06:46 +0530

    Palindrome Checker App

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+/*
+*UC1:PalindromeCheckerApp
+* @author Sriram
+* @version 1.0
+*  */
+
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System ");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+
+    }
+}
